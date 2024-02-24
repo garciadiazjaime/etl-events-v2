49e9868858f10cdb8b794befcc3b161f19d27164
Jaime <garciadiazjaime@gmail.com>
2024-02-24 11:07:16 -0600

    feat: cubbybear

---
+++ sites/cubbybear.js
@@ -0,0 +1,66 @@
+const cheerio = require('cheerio');
+const moment = require('moment');
+
+const { extract } = require('../support/extract');
+const { processEventsWithoutArtist } = require('../support/preEvents');
+
+function transform(html, venue) {
+  const $ = cheerio.load(html);
+
+  const events = $('.eventlist-event--upcoming')
+    .toArray()
+    .map((item) => {
+      const name = $(item).find('.eventlist-title').first().text()
+        .trim();
+      const description = undefined;
+      const image = $(item).find('img').data('src');
+      const _url = new URL(venue.url);
+      const url = `${_url.origin}${$(item)
+        .find('.eventlist-title a')
+        .attr('href')}`;
+      const buyUrl = undefined;
+      const price = undefined;
+
+      const date = $(item).find('.event-date').attr('datetime');
+      const time = $(item).find('.event-time-12hr-start').text().trim();
+      const dateTime = `${date} ${time}`;
+      const start_date = moment(dateTime, 'YYYY-MM-DD h:mma');
+
+      const artists = undefined;
+
+      return {
+        name,
+        description,
+        image,
+        url,
+        buyUrl,
+        price,
+        start_date,
+        artists,
+      };
+    });
+
+  return events;
+}
+
+async function main() {
+  // todo: this sites provides little information, not sure worth the scrapper
+  const venue = {
+    venue: 'The Cubby Bear Chicago',
+    provider: 'CUBBY_BEAR_CHICAGO',
+    city: 'Chicago',
+    url: 'https://www.cubbybear.com/live-music',
+  };
+
+  const html = await extract(venue.url);
+
+  const preEvents = transform(html, venue);
+
+  await processEventsWithoutArtist(venue, preEvents);
+}
+
+if (require.main === module) {
+  main().then(() => {});
+}
+
+module.exports = main;
--- support/preEvents.js
+++ support/preEvents.js
@@ -130,10 +130,35 @@ async function processEventWithArtist(venue, location, preEvent) {
   await saveEvent(event);
 }
 
+async function processEventsWithoutArtist(venue, preEvents) {
+  const location = await getGMapsLocation(venue);
+
+  if (!location) {
+    return;
+  }
+
+  await async.eachSeries(preEvents, async (preEvent) => {
+    const event = {
+      ...preEvent,
+      location,
+      provider: venue.provider,
+      venue: venue.venue,
+      city: venue.city,
+    };
+
+    console.log(JSON.stringify(event, null, 2));
+
+    await saveEvent(event);
+  });
+
+  logger.info('processed', { total: preEvents.length });
+}
+
 module.exports = {
   processEventsWithArtist,
   processEventWithArtistDetails,
   getArtistsDetails,
   processEventsWithArtistDetails,
   processEventWithArtist,
+  processEventsWithoutArtist,
 };
